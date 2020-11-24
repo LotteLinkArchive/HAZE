e191cc20056b2e7312c247ea8704a40e7a87573d
Naphtha Nepanthez <naphtha@lotte.link>
2020-11-24 22:41:53 +0000

    build Something

---
+++ src/engine/main.c
@@ -0,0 +1,13 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <SDL2/SDL_opengl.h>
+#include <GL/glu.h>
+#include <stdio.h>
+#include <string.h>
+#include "../shared/global.h"
+
+INAT main(INAT argc, CHR *argv[])
+{
+	printf("-- it works --");
+}
